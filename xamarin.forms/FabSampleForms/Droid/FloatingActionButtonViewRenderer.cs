00229fc143661089026703df69519f87408408e6
chrisriesgo <cmriesgo@gmail.com>
2015-04-19 14:42:40 -0500

    Initial commit

---
+++ xamarin.forms/FabSampleForms/Droid/FloatingActionButtonViewRenderer.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Xamarin.Forms.Platform.Android;
+using Android.Widget;
+using Xamarin.Forms;
+using FabSampleForms;
+using FabSampleForms.Droid;
+using com.refractored.fab;
+using Android.Views;
+using System.IO;
+
+[assembly: ExportRenderer (typeof (FloatingActionButtonView), typeof (FloatingActionButtonViewRenderer))]
+
+namespace FabSampleForms.Droid
+{
+	public class FloatingActionButtonViewRenderer : ViewRenderer<FloatingActionButtonView, FrameLayout>
+	{
+		private const int MARGIN_DIPS = 16;
+		private const int FAB_HEIGHT_NORMAL = 56;
+		private const int FAB_HEIGHT_MINI = 40;
+		private const int FAB_FRAME_HEIGHT_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_NORMAL;
+		private const int FAB_FRAME_WIDTH_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_NORMAL;
+		private const int FAB_MINI_FRAME_HEIGHT_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_MINI;
+		private const int FAB_MINI_FRAME_WIDTH_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_MINI;
+		private readonly Android.Content.Context context;
+		private readonly FloatingActionButton fab;
+
+		public FloatingActionButtonViewRenderer()
+		{
+			context = Xamarin.Forms.Forms.Context;
+
+			float d =  context.Resources.DisplayMetrics.Density;
+			var margin = (int)(MARGIN_DIPS * d); // margin in pixels
+
+			fab = new FloatingActionButton(context);
+			var lp = new FrameLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent);
+			lp.Gravity = GravityFlags.CenterVertical | GravityFlags.CenterHorizontal;
+			lp.LeftMargin = margin;
+			lp.TopMargin = margin;
+			lp.BottomMargin = margin;
+			lp.RightMargin = margin;
+			fab.LayoutParameters = lp;
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<FloatingActionButtonView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement != null || this.Element == null)
+				return;
+
+			if (e.OldElement != null)
+				e.OldElement.PropertyChanged -= HandlePropertyChanged;
+
+			if (this.Element != null) {
+				//UpdateContent ();
+				this.Element.PropertyChanged += HandlePropertyChanged;
+			}
+
+			Element.Show = Show;
+			Element.Hide = Hide;
+
+			SetFabImage(Element.ImageName);
+			SetFabSize(Element.Size);
+
+			fab.ColorNormal = Element.ColorNormal.ToAndroid();
+			fab.ColorPressed = Element.ColorPressed.ToAndroid();
+			fab.ColorRipple = Element.ColorRipple.ToAndroid();
+			fab.HasShadow = Element.HasShadow;
+			fab.Click += Fab_Click;
+
+			var frame = new FrameLayout(context);
+			frame.RemoveAllViews();
+			frame.AddView(fab);
+
+			SetNativeControl (frame);
+		}
+
+		public void Show(bool animate = true)
+		{
+			fab.Show(animate);
+		}
+
+		public void Hide(bool animate = true)
+		{
+			fab.Hide(animate);
+		}
+
+		void HandlePropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == "Content") {
+				Tracker.UpdateLayout ();
+			} 
+			else if (e.PropertyName == FloatingActionButtonView.ColorNormalProperty.PropertyName) 
+			{
+				fab.ColorNormal = Element.ColorNormal.ToAndroid();
+			} 
+			else if (e.PropertyName == FloatingActionButtonView.ColorPressedProperty.PropertyName) 
+			{
+				fab.ColorPressed = Element.ColorPressed.ToAndroid();
+			} 
+			else if (e.PropertyName == FloatingActionButtonView.ColorRippleProperty.PropertyName) 
+			{
+				fab.ColorRipple = Element.ColorRipple.ToAndroid();
+			}
+			else if (e.PropertyName == FloatingActionButtonView.ImageNameProperty.PropertyName) 
+			{
+				SetFabImage(Element.ImageName);
+			}
+			else if (e.PropertyName == FloatingActionButtonView.SizeProperty.PropertyName) 
+			{
+				SetFabSize(Element.Size);
+			}
+			else if (e.PropertyName == FloatingActionButtonView.HasShadowProperty.PropertyName) 
+			{
+				fab.HasShadow = Element.HasShadow;
+			}
+		}
+
+		void SetFabImage(string imageName)
+		{
+			if(!string.IsNullOrWhiteSpace(imageName))
+			{
+				try
+				{
+					var drawableNameWithoutExtension = Path.GetFileNameWithoutExtension(imageName);
+					var resources = context.Resources;
+					var imageResourceName = resources.GetIdentifier(drawableNameWithoutExtension, "drawable", context.PackageName);
+					fab.SetImageBitmap(Android.Graphics.BitmapFactory.DecodeResource(context.Resources, imageResourceName));
+				}
+				catch(Exception ex)
+				{
+					throw new FileNotFoundException("There was no Android Drawable by that name.", ex);
+				}
+			}
+		}
+
+		void SetFabSize(FloatingActionButtonSize size)
+		{
+			if(size == FloatingActionButtonSize.Mini)
+			{
+				fab.Size = FabSize.Mini;
+				Element.WidthRequest = FAB_MINI_FRAME_WIDTH_WITH_PADDING;
+				Element.HeightRequest = FAB_MINI_FRAME_HEIGHT_WITH_PADDING;
+			}
+			else
+			{
+				fab.Size = FabSize.Normal;
+				Element.WidthRequest = FAB_FRAME_WIDTH_WITH_PADDING;
+				Element.HeightRequest = FAB_FRAME_HEIGHT_WITH_PADDING;
+			}
+		}
+
+		void Fab_Click (object sender, EventArgs e)
+		{
+			var clicked = Element.Clicked;
+			if(Element != null && clicked != null)
+			{
+				clicked(sender, e);
+			}
+		}
+	}
+}
+
---
+++ xamarin.forms/FabSampleForms/README.md
@@ -0,0 +1,9 @@
+# Fab Sample in Xamarin.Forms
+
+This is a simple example showing how the Floating Action Button could be pulled into a Xamarin.Forms application.
+
+### Not Implemented Yet
+
+- AbsListView integration
+- RecyclerView integration
+- ScrollView integration
